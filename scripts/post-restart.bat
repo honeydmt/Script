d8393459cf98caf7982032f5509eeac072dfe64d
Daniel Honey <131067668+honeydmt@users.noreply.github.com>
2023-05-13 07:05:45 -0700

    Update post-restart.bat

--- scripts/post-restart.bat
+++ scripts/post-restart.bat
@@ -1,6 +1,6 @@
 @echo off
 
-echo Moving for rigth folder
+echo Moving for right folder
 echo .
 
 :: Moving for Default folder
@@ -17,9 +17,9 @@ echo Download all stuff
 echo .
 
 :: Donwload drivers and 7zip
-curl -O https://raw.githubusercontent.com/honeydmt/test/master/drivers/PHDGD_Skylake_2.9.7z
-curl -O https://raw.githubusercontent.com/honeydmt/test/master/drivers/7347_Chipset_Driver_WFF6P_WN_9.4.0.1027_A00.EXE
-curl -O https://raw.githubusercontent.com/honeydmt/test/master/drivers/Chipset_Driver_8HX85_WN32_8.1.10605.221_A01.EXE
+curl -O https://raw.githubusercontent.com/honeydmt/Script/main/drivers/PHDGD_Skylake_2.9.7z
+curl -O https://raw.githubusercontent.com/honeydmt/Script/main/drivers/7347_Chipset_Driver_WFF6P_WN_9.4.0.1027_A00.EXE
+curl -O https://raw.githubusercontent.com/honeydmt/Script/main/drivers/Chipset_Driver_8HX85_WN32_8.1.10605.221_A01.EXE
 curl -O https://www.7-zip.org/a/7zr.exe
 
 echo Run chipset drivers
@@ -62,10 +62,26 @@ echo .
 mkdir post-install
 cd post-install
 curl -O https://raw.githubusercontent.com/abbodi1406/KMS_VL_ALL_AIO/master/KMS_VL_ALL_AIO.cmd
-curl -O https://us6-dl.techpowerup.com/files/XdJd_TAsQm61wzILr9Xkng/1683876667/Visual-C-Runtimes-All-in-One-Feb-2023.zip
 curl -O https://download.microsoft.com/download/1/7/1/1718CCC4-6315-4D8E-9543-8E28A4E18C4C/dxwebsetup.exe
+curl -O https://raw.githubusercontent.com/honeydmt/Script/main/progams/VisualCppRedist_AIO_x86_x64.exe
 
-:: for install i gonna do it for myself 
+echo Using KMS VL ALL AIO to activate windows
+echo .
+
+:: Open KMS VL ALL AIO
+"C:\Users\Daniel Honey\Downloads\scripts\post-install\KMS_VL_ALL_AIO.cmd"
+
+echo Install DirectX
+echo .
+
+:: Open dxwebsetup
+"C:\Users\Daniel Honey\Downloads\scripts\post-install\dxwebsetup.exe"
+
+echo Run VisualCppRuntime
+echo .
+
+:: Visual-C-Runtimes
+"C:\Users\Daniel Honey\Downloads\scripts\post-install\VisualCppRedist_AIO_x86_x64.exe
 
 echo Disable safe mode
 echo .
